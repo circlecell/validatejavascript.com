ab2b19ed13e85a4131a9ace04f17cf4c0777f863
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-10-18 16:08:18 +0300

    chore: Error at webpackify

--- tools/webpackify-eslint.js
+++ tools/webpackify-eslint.js
@@ -1,5 +1,6 @@
 const { cp, rm, test, mkdir, find } = require('shelljs');
 const path = require('path');
+const fs = require('fs');
 
 const WEBPACK_DIR = 'eslint/webpack/';
 const ESLINT_DIR = 'eslint/';
@@ -20,7 +21,8 @@ function generateRulesIndex(basedir) {
     });
 
     output += '\n    return rules;\n};';
-    output.to(`${basedir}load-rules.js`);
+
+    fs.writeFileSync(`${basedir}load-rules.js`, output)
 }
 
 function webpackify() {
