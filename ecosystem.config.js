1e0f9c564004dda4e47ae4ca798806bc2a67e72f
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2017-07-04 20:55:42 +0300

    fix: Add text to footer; rename project

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
         {
             name: 'validatejavascript.com',
             script: 'packages/backend/index.js',
-            error_file: '/var/web/eslint.io/logs/err.log',
-            out_file: '/var/web/eslint.io/logs/out.log',
+            error_file: '/var/web/validatejavascript.com/logs/err.log',
+            out_file: '/var/web/validatejavascript.com/logs/out.log',
             env: {
                 NODE_ENV: 'production',
                 PORT: '5004'
@@ -19,8 +19,8 @@ module.exports = {
             user: 'deployer',
             host: '192.241.157.86',
             ref: 'origin/master',
-            repo: 'git@github.com:circlecell/eslint.io.git',
-            path: '/var/web/eslint.io',
+            repo: 'git@github.com:circlecell/validatejavascript.com.git',
+            path: '/var/web/validatejavascript.com',
             ssh_options: ['StrictHostKeyChecking=no', 'PasswordAuthentication=no'],
             'post-deploy': 'npm run install-all && npm run build && npm start'
         }
